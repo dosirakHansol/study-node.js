0bb4c0090ed6a64a3819579b8b061616937fbe1b
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-02-07 21:35:26 +0900

    파일 수정

--- studyOne/main.js
+++ studyOne/main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url'); // 모듈 url
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>    
@@ -47,7 +47,10 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, fileList){
 
           var list = templateList(fileList);
-          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+          var template = templateHTML(title, list, 
+            `<h2>${title}</h2><p>${description}</p>`,
+            `<a href="/create">create</a>`,''
+            );
 
           response.writeHead(200);
           response.end(template);
@@ -58,7 +61,10 @@ var app = http.createServer(function(request,response){
           fs.readdir('./data', function(error, fileList){
             
             var list = templateList(fileList);
-            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+            var template = templateHTML(title, list,
+               `<h2>${title}</h2><p>${description}</p>`,
+               `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+               );
 
             response.writeHead(200);
             response.end(template);
@@ -72,7 +78,7 @@ var app = http.createServer(function(request,response){
 
           var list = templateList(fileList);
           var template = templateHTML(title, list, `
-            <form action="http://localhost:3000/create_process" method="post">
+            <form action="/create_process" method="post">
               <p><input type="text" name="title" placeholder="title"></p>
               <p>
                 <textarea name="description" placeholder="description"></textarea>
@@ -101,6 +107,52 @@ var app = http.createServer(function(request,response){
             response.end('success');
           })
         });
+
+    } else if(pathname === '/update'){
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        fs.readdir('./data', function(error, fileList){
+          
+          var list = templateList(fileList);
+          var template = templateHTML(title, list,
+             `
+             <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+             `,
+             `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+             );
+
+          response.writeHead(200);
+          response.end(template);
+        })
+       });
+    } else if(pathname === '/update_process'){
+      var body ='';
+      request.on('data', function(data){
+        body += data;
+      });
+
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+
+        fs.rename(`data/${id}`, `data/${title}`, function(err){
+          fs.writeFile(`data/${title}`, description, 'utf8', function (err){
+            response.writeHead(302, {Location : `/?id=${title}`});
+            response.end('success');
+          })
+        })
+        console.log(post);
+      });
 
     } else {
         response.writeHead(404);
