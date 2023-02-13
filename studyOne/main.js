cfeca7d3053a699e24ca6c966789d952a8be4596
DESKTOP-69UC1S4\박한솔 <crying_beg@naver.com>
2023-02-13 22:20:09 +0900

    nodejs - npm 사용 -  API

--- studyOne/main.js
+++ studyOne/main.js
@@ -4,22 +4,24 @@ var url = require('url'); // 모듈 url
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
+const sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query; // url에서 쿼리스트링 가져오기
-    var title = queryData.id;
-    var pathname = url.parse(_url,true).pathname //root 경로인듯
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query; // url에서 쿼리스트링 가져오기
+  var title = queryData.id;
+  var pathname = url.parse(_url,true).pathname //root 경로인듯
 
     if(pathname === '/'){
       if(queryData.id === undefined) {
         title = 'welcome kkk';
-        description = 'hello, node.js!'
-
+        description = 'hello, node.js!';
+        var filteredTitle = sanitizeHtml(title);
+        var filteredDes = sanitizeHtml(description);
         fs.readdir('./data', function(error, fileList){
           var list = template.list(fileList);
-          var html = template.HTML(title, list, 
-              `<h2>${title}</h2><p>${description}</p>`,
+          var html = template.HTML(filteredTitle, list, 
+              `<h2>${filteredTitle}</h2><p>${filteredDes}</p>`,
               `<a href="/create">create</a>`,''
               );
           response.writeHead(200);
@@ -30,14 +32,21 @@ var app = http.createServer(function(request,response){
         var fileteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${fileteredId}`, 'utf8', function(err, description){
           fs.readdir('./data', function(error, fileList){
-            
             var list = template.list(fileList);
-            var html = template.HTML(title, list,
-               `<h2>${title}</h2><p>${description}</p>`,
+            var filteredTitle = sanitizeHtml(title);
+            var filteredDes = sanitizeHtml(description,{
+              allowedTags: [ 'b', 'i', 'em', 'strong', 'a', 'h1' ],
+              allowedAttributes: {
+                'a': [ 'href' ]
+              },
+              allowedIframeHostnames: ['www.youtube.com']
+            });
+            var html = template.HTML(filteredTitle, list,
+               `<h2>${filteredTitle}</h2><p>${filteredDes}</p>`,
                `<a href="/create">create</a> 
-               <a href="/update?id=${title}">update</a>
+               <a href="/update?id=${filteredTitle}">update</a>
                <form action="delete_process" method="post">
-                <input type="hidden" name="id" value="${title}"/>
+                <input type="hidden" name="id" value="${filteredTitle}"/>
                 <input type="submit" value="delete"/>
                </form>
                `
@@ -145,8 +154,8 @@ var app = http.createServer(function(request,response){
         })
       });
     } else {
-        response.writeHead(404);
-        response.end('not found lol');
+      response.writeHead(404);
+      response.end('not found lol');
     } 
       
 });
